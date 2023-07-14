7fbbdc3c2c4d95684a1497542b1b43e7edcae4eb
welchm27 <welchm27@gmail.com>
2023-07-14 14:03:16 +0000

    71 Best practice and pylint complete

---
+++ smallscript.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Author: Michael
+
+This program prints some things to the screen
+"""
+
+def main():
+    """ our main program"""
+    mynote = "print this string"  # define mynote
+    print(mynote)                 # display our string
+
+main()
